be3fd71b66f991c63e64993bddacc64ed7ef9069
erkkah <erik.agsjo@gmail.com>
2016-07-04 22:57:57 +0200

    Delayed file loading using generators

--- gr8TRACKer.py
+++ gr8TRACKer.py
@@ -14,12 +14,19 @@ Options:
 from docopt import docopt
 from pydub import AudioSegment
 from os import path
-from glob import glob
+from glob import iglob
+from itertools import chain
 
 # http://www.geeksforgeeks.org/bin-packing-problem-minimize-number-of-used-bins/
 # http://moose.cs.ucla.edu/publications/schreiber_korf_ijcai13.pdf
 
-def pack_tracks(files, tracklen, padding, fadelen):
+def s_to_minsec(s):
+    rounded = round(s)
+    min = int(rounded // 60)
+    sec = int(rounded % 60)
+    return "{}:{}".format(min, sec)
+
+def pack_tracks(segments, tracklen, padding, fadelen):
     # Tracks start and end with padded silence
     track_time_seconds = tracklen * 60 - (padding * 2)
     padding_ms = padding * 1000
@@ -29,14 +36,17 @@ def pack_tracks(files, tracklen, padding, fadelen):
     track_time_left = track_time_seconds
     track_count = 1
 
-    for file in files:
-        if track_time_left > file.duration_seconds:
-            master += file
-            track_time_left -= file.duration_seconds
+    for file, segment in segments:
+        print("Adding {}".format(file), end = "")
+        if track_time_left > segment.duration_seconds:
+            master += segment
+            track_time_left -= segment.duration_seconds
+            print(" [complete]")
         else:
+            print(" [cut @ {}]".format(s_to_minsec(track_time_left)))
             split_point = track_time_left * 1000
-            first_half = file[:split_point]
-            second_half = file[split_point:]
+            first_half = segment[:split_point]
+            second_half = segment[split_point:]
             master += first_half.fade_out(fadelen_ms)
             master += AudioSegment.silent(duration = padding_ms)
             track_count += 1
@@ -47,34 +57,36 @@ def pack_tracks(files, tracklen, padding, fadelen):
             master += second_half.fade_in(fadelen_ms)
             track_time_left = track_time_seconds - second_half.duration_seconds
 
-    print("Build {} tracks, total length {}s".format(track_count, master.duration_seconds))
+    total = master.duration_seconds
+    print("Built {} tracks, total length {}".format(track_count, s_to_minsec(total)))
     return master
 
+def file_segments(sourcedir):
+    pattern = path.join(sourcedir, "*")
+    matching = sorted(iglob(pattern))
+    for file in matching:
+        types = ["mp4", "aac", "mp3", "wav", "ogg"]
+        for type in types:
+            if file.endswith(type):
+                yield([file, AudioSegment.from_file(file, type)])
+                break
+
 def build_from_dir(sourcedir, target, tracklen, padding, fadelen, pitch):
-    types = ["mp4", "aac", "mp3", "wav", "ogg"]
-    all_files = []
-    for type in types:
-        ext = "*." + type
-        pattern = path.join(sourcedir, ext)
-        all_files += [AudioSegment.from_file(file, type) for file in glob(pattern)]
+    segments = file_segments(sourcedir)
 
-    total_len = 0
-    for file in all_files:
-        total_len += file.duration_seconds
-
-    total_len_minutes = total_len / 60
-    print("Loaded {} files\n".format(len(all_files)))
-    print("Len: {}".format(total_len_minutes))
-
+    print("Packing...")
     master = pack_tracks(
-        files = all_files,
+        segments = segments,
         tracklen = tracklen,
         padding = padding,
         fadelen = fadelen)
 
     # Do pitching here!
 
+    print("Writing track...")
     master.export(target, format = "wav")
+
+    print("Done!")
 
 def main():
     arguments = docopt(__doc__, version = 'gr8tracker 0.5')
